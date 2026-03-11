c736e49ef3278a76b56c42da7f35afa241f79a31
Mayank kumar <mayankkumarsharma244@gmail.com>
2026-03-11 21:20:16 +0530

    Add files via upload

---
+++ majority.cpp
@@ -0,0 +1,57 @@
+//Brute force method
+
+
+int majorityElements(vector<int> &nums){
+    int n = nums.size();
+    for(int val : nums){
+        int freq = 0;
+        for(int el : nums){
+            if(el == val){
+                freq++;
+            }
+        }
+        if(freq > n/2){
+            return val;
+        }
+    }
+}
+
+//optimal method (by sorting)
+//TC:O(nlogn)
+int majorityElements(vector<int> &nums){
+    int n = nums.size();
+
+    sort(nums.begin(), nums.end());
+    int freq = 1, ans = nums[0];
+    for(int i=1; i<n; i++){
+        if(nums[i] == nums[i-1]){
+            freq++;
+        }else {
+            freq=1;
+            ans = nums[i];
+        }
+        if(freq > n/2){
+            return ans;
+        }
+    }
+
+    return ans;
+}
+
+//Best method (MOORE'S VOTING ALGORITHM)
+int majorityElements(vector<int> &nums){
+    int freq = 0, ans = 0;
+
+    for(int i=0 ; i<n; i++){
+        if(freq == 0){
+            ans = nums[i];
+        }
+        if(ans == nums[i]){
+            freq++;
+        }else{
+            freq--;
+        }
+    }
+    return ans;
+}
+
